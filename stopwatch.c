bacbdc7699fd36e69d97a90d00a3a9b81b58c2c1
Kenoooo <keno.peil@gmx.de>
2018-10-30 21:27:15 +0100

    added type cast

--- stopwatch.c
+++ stopwatch.c
@@ -10,7 +10,7 @@ void getTime(char** returnValue){
 
 	char* str;
 	str = malloc(20 * sizeof(char));
-	unsigned long long millis = tval.tv_sec * 1000 + tval.tv_usec / 1000;
+	unsigned long long millis = (unsigned long long) tval.tv_sec * 1000 + tval.tv_usec / 1000;
 	sprintf(str, "%lld", millis);
 	
 	strcpy(*returnValue, str);
